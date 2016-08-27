1b983269f948b05e9368454a194f807759551116
Raunak Kathuria <raunakkathuria@gmail.com>
2016-08-27 17:32:04 +0800

    solve problem 76 of project euler - Counting summations

--- src/js/combinedprojecteulersolutions.js
+++ src/js/combinedprojecteulersolutions.js
@@ -760,6 +760,46 @@
     console.log(">>>>>>> ------------------------------------------------------------------------------ <<<<<<<");
 
 /*
+    https://projecteuler.net/problem=76
+    ----------------------------------
+    Counting summations
+
+    Problem Statement:
+    It is possible to write five as a sum in exactly six different ways:
+
+        4 + 1
+        3 + 2
+        3 + 1 + 1
+        2 + 2 + 1
+        2 + 1 + 1 + 1
+        1 + 1 + 1 + 1 + 1
+    How many different ways can one hundred be written as a sum of at least two positive integers?
+*/
+
+    /*
+     * This problem is similar to problem31, instead of coins we have
+     * numbers till 100 as selection criteria to find combinations
+     * please refer src/js/projecteulerproblem31.js for more details
+     */
+    function countingSummations(target) {
+        let numWays = new Array(target+1).fill(0);
+        numWays[0] = 1;
+
+        for (let i = 1; i <= 99; i++) {
+            for (let j = i; j <= target; j++) {
+                numWays[j] += numWays[j - i];
+            }
+        }
+
+        return numWays[target];
+    }
+
+    console.log("#### Problem76 (https://projecteuler.net/problem=76) ####");
+    profile = profiler(countingSummations);
+    console.log("       counting summations for 100 result is", profile.call(null, 100));
+    console.log(">>>>>>> ------------------------------------------------------------------------------ <<<<<<<");
+
+/*
     https://projecteuler.net/problem=81
     ----------------------------------
     Path sum: two ways
